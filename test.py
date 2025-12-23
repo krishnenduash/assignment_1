56422e01f4c38fc40aa79eadbc467f15c268e895
Mastermind <30100922+krishnenduash@users.noreply.github.com>
2025-12-23 20:11:14 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+"""it is a python Script"""
+print('my first python program')
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name : ")
+print("Hello,", first_name, last_name+'!', 'Welcome to my first python program.')
