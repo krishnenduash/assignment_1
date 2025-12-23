62ad270ca1a51ec877c2830e83f2ca47826a3cb3
Mastermind <30100922+krishnenduash@users.noreply.github.com>
2025-12-23 20:28:28 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,8 @@
+print('My second program')
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+print('Addition: ',first_number + second_number)
+print('Subtraction: ',first_number - second_number)
+print('Multiplication: ',first_number * second_number)
+print('Division: ',first_number / second_number)
